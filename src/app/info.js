a1285c1cd2b60cfbf4ee1faa89801c68b77c3ea7
angelspacenter111 <angel.spacenter111@gmail.com>
2026-04-11 18:12:30 +0530

    change base URL

--- src/app/info.js
+++ src/app/info.js
@@ -1,9 +1,13 @@
 const name = "Lotus Spa";
 const contact = "+91 9967313968";
-const base_url = 'http://localhost:3000/';
-const image_url = 'http://localhost:3000/assets/';
 
+// Live
+const base_url = 'https://lotusspamalad.vercel.app/';
+const image_url = 'https://lotusspamalad.vercel.app/assets/';
 
+// Local
+// const base_url = 'http://localhost:3000/';
+// const image_url = 'http://localhost:3000/assets/';
 
 export {
     name, contact, base_url, image_url
