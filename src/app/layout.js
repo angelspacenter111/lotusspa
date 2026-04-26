d370ff42ac55dc85018d3811d04f5731a4c81307
angelspacenter111 <angel.spacenter111@gmail.com>
2026-04-26 16:53:39 +0530

    improve SEO

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
 				<meta name="geo.placename" content="Mumbai" />
 
 
-				<link rel="icon" type="image/x-icon" href="http://localhost:3000/assets/logot.png"></link>
+				<link rel="icon" href="https://lotusspainmalad.vercel.app/logot.png"></link>
 				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
 				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
 				<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet" />
